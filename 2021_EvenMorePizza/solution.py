b2f6661bd80be58ae07db1362c6afaca56ccf921
LinaM20 <c19366191@mytudublin.ie>
2021-02-15 17:26:54 +0000

    Edited main fully

--- 2021_EvenMorePizza/solution.py
+++ 2021_EvenMorePizza/solution.py
@@ -32,31 +32,34 @@ def deliveries(pizzaAmount, teamTwo, teamThree, teamFour, the_pizza_dict):
     
 
     # delivering pizza to each team
-    for pizza in the_pizza_dict.values():
-        # delivering to teams of 2 
-        while numPizza < two_team_total:
-            order_list1.append(the_pizza_dict[numPizza]) 
+    for pizza in the_pizza_dict.keys():
+
+        #delivering to teams of 2 
+        if numPizza < two_team_total:
+
+            order_list1.append(pizza) 
             pizzaAmount = int(pizzaAmount) - 1
             print("\nAmount of pizzas left: " + str(pizzaAmount))
             numPizza += 1
+
 
         # delivering to teams of 3
-        while numPizza != int(max_pizza):
-            order_list2.append(the_pizza_dict[numPizza]) 
-            pizzaAmount = int(pizzaAmount) - 1
-            print("\nAmount of pizzas left: " + str(pizzaAmount))
-            numPizza += 1
+        # while numPizza != int(max_pizza):
+        #     order_list2.append(the_pizza_dict[numPizza]) 
+        #     pizzaAmount = int(pizzaAmount) - 1
+        #     print("\nAmount of pizzas left: " + str(pizzaAmount))
+        #     numPizza += 1
 
         # delivering teams of 4
-        while numPizza != int(max_pizza):
-            order_list3.append(the_pizza_dict[numPizza])
-            pizzaAmount = int(pizzaAmount) - 1
-            print("\nAmount of pizzas left: " + str(pizzaAmount))
-            numPizza += 1
+        # while numPizza != int(max_pizza):
+        #     order_list3.append(the_pizza_dict[numPizza])
+        #     pizzaAmount = int(pizzaAmount) - 1
+        #     print("\nAmount of pizzas left: " + str(pizzaAmount))
+        #     numPizza += 1
         
     # No more pizza left
-    if int(pizzaAmount) == 0:
-        print("Sorry, we could not deliver your pizza :(\n")
+    # if int(pizzaAmount) == 0:
+    #     print("Sorry, we could not deliver your pizza :(\n")
 
 
     # printing pizza per team
@@ -68,68 +71,62 @@ def deliveries(pizzaAmount, teamTwo, teamThree, teamFour, the_pizza_dict):
     print("\nTEAM THREE PIZZA:")
     print(order_list2)
 
-    print("\nTEAM FOUR PIZZA:")
-    print(order_list3)
+    # print("\nTEAM FOUR PIZZA:")
+    # print(order_list3)
 
 
-    # writing to a file to create an out file
-    with open("C:\\Users\\Windows\\Desktop\\CS\\HASHCODE\\Practice\\example.out", "w", encoding='utf8') as outputfile: # unicode transformation formatos xx
-        outputfile.write(str(order_list1)) 
-        outputfile.write("\n")
+    # # writing to a file to create an out file
+    # with open("C:\\Users\\Windows\\Desktop\\Hashcode_Repo\\2021_EvenMorePizza\\output\\a_example.out", "w", encoding='utf8') as outputfile: # unicode transformation formatos xx
+    #     outputfile.write(str(order_list1)) 
+    #     outputfile.write("\n")
 
-        outputfile.write(str(order_list2))
-        outputfile.write("\n")
-        # hehe
-        outputfile.write(str(order_list3))
+    #     outputfile.write(str(order_list2))
+    #     outputfile.write("\n")
+    
+    #     outputfile.write(str(order_list3))
 
 
 def main():
-    # Reading the input file 
-    fp = open("C:\\Users\\Windows\\Desktop\\CS\\HASHCODE\\Practice\\a_example", "r") 
     details = []
     counter = 0
-
-    for line in fp.readline():
-        for num in line.split(): 
-            details.append(num)
-
-    print(details)
-    print("\n")
-
-    availablePizza = details[0]
-
-    twoPersonTeam = details[1]
-
-    threePersonTeam = details[2]
-
-    fourPersonTeam = details[3]
-    
-    print("Number of pizza available is: " + availablePizza)
-
-    print("Number of two-Person teams is: " + twoPersonTeam)
-
-    print("Number of three-Person teams is: " + threePersonTeam)
-
-    print("Number of four-Person teams is: " + fourPersonTeam)
-    print("\n")
-
-    # print(fp.read())
-
-    fp.close()
 
     pizza_dict = {}
     pizzaNum = 0
 
-    with open("C:\\Users\\Windows\\Desktop\\CS\\HASHCODE\\Practice\\a_example", "r") as file: 
-        next(file) # skipping the first line
-        # reading each line from the input file 
+    # Reading the input file
+    with open("C:\\Users\\Windows\\Desktop\\Hashcode_Repo\\2021_EvenMorePizza\\input\\a_example", "r") as file:
+        
+        for line in file.readline():
+            for num in line.split(): 
+                details.append(num)
+
+        print(details)
+        print("\n")
+
+        availablePizza = details[0]
+
+        twoPersonTeam = details[1]
+
+        threePersonTeam = details[2]
+
+        fourPersonTeam = details[3]
+        
+        print("Number of pizza available is: " + availablePizza)
+
+        print("Number of two-Person teams is: " + twoPersonTeam)
+
+        print("Number of three-Person teams is: " + threePersonTeam)
+
+        print("Number of four-Person teams is: " + fourPersonTeam)
+        print("\n")
+
         print("Pizza list:\n")
 
         for line in file:
             line = line.strip('\n') # strip removed the \n character from the line
             pizza_dict[pizzaNum] = line
             pizzaNum += 1
-            
+                
         print(pizza_dict)
 
         deliveries(availablePizza, twoPersonTeam, threePersonTeam, fourPersonTeam, pizza_dict)
